869e9e39245b311d203506a3bc141512869cbf38
bhagirathi <bhagirathi.muduli@infogain.com>
2016-04-16 18:55:44 +0530

    modified to infogain

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test
 {
   public static void main (String arg[])
   {
-    System.out.println("Welcome Java for GIT Test");
+    System.out.println("Welcome Java for GIT Test of Infogain");
   }
 }
